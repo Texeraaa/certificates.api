0855ffa7bd1febe651298703a9cf225a88d62b4f
Pedro Teixeira <ph.teixeira.ph2020@gmail.com>
2023-10-25 20:07:43 -0300

    feat(Server):`server`: fazendo server

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,15 @@
-import express from 'express';
-const app = express();
-const PORT = 3000
+const express = require('express');
+const App = require("./src/app.js");
+const path = require('path')
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+const PORT = process.env.PORT
 
-app.listen(3000, () => 
-console.log(`Servidor iniciado na porta http://localhost:${PORT}`)
-);
+try {
+    let app = App.iniciar()
+    app.listen(PORT, () => 
+    console.log(`Servidor iniciado na porta http://localhost:${PORT}`)
+    );
+    
+} catch (error) {
+    console.log(error);
+}
