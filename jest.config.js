913fd8273e130d38cb39093ce9c1d458782f1455
Pedro Teixeira <pedro.teixeira@gruponsc.com.br>
2023-11-20 22:38:05 -0300

    configurando docker e jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// jest.config.js
+module.exports = {
+  testMatch: [
+    "**/__tests__/**/*.js", // Padrão para arquivos de teste
+    "**/?(*.)+(spec|test).js", // Padrão para arquivos de teste com sufixo
+  ],
+};
