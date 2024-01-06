9deff41bcf1e50bf83df9fb875ab97a39cffd94d
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-01-06 02:56:38 -0300

    feat(Participant):`participant-entity`: creating a entity participant

---
+++ src/Participant/participant.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'Participante'})
+export class ParticipantEntity{
+    @PrimaryGeneratedColumn('uuid')
+    idParticipante: string
+
+    @Column({
+        length: 255,
+        nullable: false
+    })
+    nomeParticipante: string
+
+    @Column({
+        length: 11,
+        nullable: false
+    })
+    cpfParticipante: string
+
+    @Column({
+        length:11,
+        nullable: false
+    })
+    telefoneParticipante: string
+
+    @Column({
+        length: 255,
+        nullable: false
+    })
+    emailParticipante: string
+
+    @Column({
+        length: 255,
+        nullable: false
+    })
+    senhaParticipante: string
+}
