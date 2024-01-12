791cdbb12b44ed68cecc476942f85c79997fd510
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-01-12 14:53:57 -0300

    feat: entity tipoPapel

---
+++ src/participant-event/entity/participant-event.entity.ts
@@ -0,0 +1,21 @@
+import { EventEntity } from "src/event/entity/event.entity";
+import { ParticipantEntity } from "src/participant/entity/participant.entity";
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class ParticipantEventEntity{
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @ManyToOne(() => ParticipantEntity)
+    @JoinColumn({name: 'idParticipante'})
+    idParticipante: ParticipantEntity
+
+    @ManyToOne(() => EventEntity)
+    @JoinColumn({name: 'idEvento'})
+    idEvento: EventEntity
+
+    @ManyToOne(() => ParticipantEventEntity)
+    @JoinColumn({name: 'idTipoPapel'})
+    idTipoPapel: ParticipantEventEntity
+}
