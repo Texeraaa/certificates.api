f96f5bb8864b1b1fbd3dd5fd62a53b893e2e59f6
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-01-23 11:29:48 -0300

    feat(participant):`participant.controller`: making CRUD participant

--- src/participant/participant.controller.ts
+++ src/participant/participant.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from "@nestjs/common";
 import { ParticipantService } from "./participant.service";
+import { CreateParticipantDTO } from "./dto/create-participant.dto";
+import { ParticipantEntity } from "./entity/participant.entity";
+import { UpdateParticipantDTO } from "./dto/update-participant.dto";
 
-@Controller('/participant')
+@Controller('/participants')
 export class ParticipantController {
     constructor(private participantService: ParticipantService){}
 
     @Get()
     async readParticipants(){
-        return this.participantService.readParticipant()
+        const res = await this.participantService.readParticipant()
+        return res
+    }
+    
+    @Post()
+    async createParticipant(@Body() createParticipantDTO: CreateParticipantDTO){
+        const participant = await this.participantService.createParticipant(createParticipantDTO)
+        return {
+            status: 201,
+            message: 'Participante cadastrado com sucesso',
+            return:{
+                id: participant.idParticipante,
+                name: participant.nomeParticipante
+            } 
+        }
+    }
+
+    @Put('/:id')
+    async updateParticipant(@Param('id') id: string,@Body() newData: UpdateParticipantDTO){
+        await this.participantService.updateParticipant(id,newData)
+        return {
+            status: 200,
+            message: 'Participante editado com sucesso',
+            return: newData
+        }
+    }
+
+    @Delete('/:id')
+    async deleteParticipant(@Param('id') id: string){
+        await this.participantService.deleteParticipant(id)
+        return{
+            status: 200,
+            message: 'Participante deletado com sucesso',
+        }
     }
 }
