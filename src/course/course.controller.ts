ff076348ca4cecfe0aa94d85baf490d09c46a88a
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-03-20 21:26:05 -0300

    feat(course):`course.controller`: documentando reqs

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,26 +1,33 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Post } from "@nestjs/common";
 import { CourseService } from "./course.service";
 import { CreateCourseDTO } from "./dto/createCourse.dto";
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { CourseResponseDTO } from "./dto/courseResponse.dto";
 
+@ApiTags('Course')
 @Controller('/course')
 export class CourseController{
     constructor(
         private courseService: CourseService
-    ){}
+    ){} 
     
     @Get()
-    async readCourse(){
+    @ApiResponse({status: 200, description: 'Retorna uma lista de cursos cadastrados', type: CourseResponseDTO, isArray: true})
+    async read(){
         const response = await this.courseService.readCourse()
         return response
     }
 
     @Post()
     @HttpCode(201)
-    async createCourse(@Body() createCourseDTO: CreateCourseDTO){
+    @ApiOperation({summary: 'Criar um novo curso'})
+    @ApiResponse({status:201,description: 'Curso criado com sucesso!'})
+    @ApiBody({type: CreateCourseDTO,description: 'Dados do curso'})
+    async create(@Body() createCourseDTO: CreateCourseDTO){
         const response = await this.courseService.createCourse(createCourseDTO)
         return{
             status: 201,
-            message: 'Curso criado com sucesso',
+            message: 'Curso criado com sucesso!',
             return:{
                 id: response.idCurso,
                 curso: response.nomeCurso
@@ -29,7 +36,10 @@ export class CourseController{
     }
     
     @Delete('/:id')
-    async deleteCourse(@Param('id') id: string){
+    @ApiOperation({summary: 'Deleta um curso pelo ID'})
+    @ApiResponse({status: 200,description: 'Curso deletado com sucesso'})
+    @ApiParam({name: 'id', description: 'ID do curso a ser deletado',type: String})
+    async delete(@Param('id') id: string){
         await this.courseService.deleteCourse(id)
         return{
             status: 200,
