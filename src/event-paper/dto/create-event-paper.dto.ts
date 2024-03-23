51bc529a26e609d00f7e863ff54317a6d672bbe1
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-03-23 15:42:58 -0300

    doc: documentando event-papaer e event

--- src/event-paper/dto/create-event-paper.dto.ts
+++ src/event-paper/dto/create-event-paper.dto.ts
@@ -1,7 +1,13 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class CreateEventPaperDTO{
-    @IsString({message: 'O campo nomeTipoPapel deve ser do tipo string'})
-    @IsNotEmpty({message: 'O campo nomeTipoPapel deve ser preenchido'})
-    nomeTipoPapel: string
+export class CreateEventPaperDTO {
+    @IsString({ message: 'O campo nomeTipoPapel deve ser do tipo string' })
+    @IsNotEmpty({ message: 'O campo nomeTipoPapel deve ser preenchido' })
+    @ApiProperty({
+        example: 'Palestrante',
+        description: 'Nome do tipo de papel relacionado ao evento, por exemplo, Palestrante e alunos.',
+        type: String
+    })
+    nomeTipoPapel: string;
 }
--- src/event/event.controller.ts
+++ src/event/event.controller.ts
@@ -4,17 +4,25 @@ import { EventEntity } from "./entity/event.entity";
 import { CreateEventDTO } from "./dto/create-event.dto";
 import { v4 as uuid } from "uuid"
 import { UpdateEventDTO } from "./dto/update-event.dto";
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { EventResponseDTO } from "./dto/eventResponse.dto";
 
+@ApiTags('Event')
 @Controller('/events')
 export class EventController{
     constructor(private eventService: EventService){}
 
+
     @Get()
-    async readEvents(){
+    @ApiResponse({status: 200, description: 'Retorna uma lista de cursos cadastrados', type: EventResponseDTO, isArray: true})
+    async read(){
         return await this.eventService.readEvents()
     }
 
     @Post()
+    @ApiOperation({summary: 'Criar um novo evento'})
+    @ApiResponse({status:201,description: 'evento cadastrado com sucesso'})
+    @ApiBody({type: CreateEventDTO,description: 'Dados do evento'})
     async createEvent(@Body() criaEventoDto: CreateEventDTO){
         const eventEntity = new EventEntity()
         eventEntity.idEvento = uuid()
@@ -22,6 +30,7 @@ export class EventController{
         eventEntity.dataEvento = criaEventoDto.dataEvento
         eventEntity.horaEvento = criaEventoDto.horaEvento
         eventEntity.cargaHorariaEvento = criaEventoDto.cargaHorariaEvento
+        eventEntity.quantidadeDeAlunos = criaEventoDto.quantidadeDeAlunos
         eventEntity.status = criaEventoDto.status
 
         await this.eventService.createEvent(eventEntity)
@@ -34,6 +43,10 @@ export class EventController{
     }
     
     @Put('/:id')
+    @ApiOperation({ summary: 'Edita um evento existente' })
+    @ApiResponse({ status: 200, description: 'Evento atualizado com sucesso' })
+    @ApiParam({ name: 'id', type: String, description: 'ID do evento a ser atualizado' })
+    @ApiBody({ type: UpdateEventDTO, description: 'Dados para atualização do evento' })
     async updateEvent(@Param('id') id: string, @Body() newData: UpdateEventDTO){
         await this.eventService.updateEvent(id,newData)
         return{
@@ -44,6 +57,9 @@ export class EventController{
     }
 
     @Delete('/:id')
+    @ApiOperation({ summary: 'Deleta um evento existente' })
+    @ApiResponse({ status: 200, description: 'Evento deletado com sucesso' })
+    @ApiParam({ name: 'id', type: String, description: 'ID do evento a ser deletado' })
     async deleteEvent(@Param('id') id: string){
         await this.eventService.deleteEvent(id)
         return{
