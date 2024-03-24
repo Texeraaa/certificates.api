cf593a898e379b6e4ccfcc4be609fa407dd302e6
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-03-24 03:40:37 -0300

    eslint

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,24 +1,23 @@
-import { InjectRepository } from "@nestjs/typeorm";
-import { CourseEntity } from "./entity/course.entity";
-import { Repository } from "typeorm";
-import { CreateCourseDTO } from "./dto/createCourse.dto";
+import { InjectRepository } from '@nestjs/typeorm';
+import { CourseEntity } from './entity/course.entity';
+import { Repository } from 'typeorm';
+import { CreateCourseDTO } from './dto/createCourse.dto';
 
-export class CourseService{
-    constructor(
-        @InjectRepository(CourseEntity)
-        private readonly courseService: Repository<CourseEntity>
-    ){}
+export class CourseService {
+  constructor(
+    @InjectRepository(CourseEntity)
+    private readonly courseService: Repository<CourseEntity>,
+  ) {}
 
-    async readCourse(){
-        return await this.courseService.find()
-         
-    }
+  async readCourse() {
+    return await this.courseService.find();
+  }
 
-    async createCourse(createCourseDTO: CreateCourseDTO){
-        return await this.courseService.save(createCourseDTO)
-    }
+  async createCourse(createCourseDTO: CreateCourseDTO) {
+    return await this.courseService.save(createCourseDTO);
+  }
 
-    async deleteCourse(idCurso: string){
-        return await this.courseService.delete(idCurso)
-    }
+  async deleteCourse(idCurso: string) {
+    return await this.courseService.delete(idCurso);
+  }
 }
